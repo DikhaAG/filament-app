07465da8e65828150aeced0b303dcc9bff889404
DikhaAG <dikhaagsfrdy@gmail.com>
2025-12-15 22:50:56 +0700

    filament>resource: menambahkan kolom email_verified_at pada table user

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -15,7 +16,25 @@ class UsersTable
         return $table
             ->columns([
                 TextColumn::make("name"),
-                TextColumn::make("email"),
+                TextColumn::make("email")->icon(Heroicon::Envelope),
+                TextColumn::make("email_verified_at")
+                    ->label("Terverifikasi")
+                    ->badge(function ($value) {
+                        if (is_null($value)) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    })
+                    ->state(fn($value) => is_null($value) ? "Belum Terverifikasi" : $value)
+                    ->color(function ($value) {
+                        if (is_null($value)) {
+                            return "danger";
+                        }
+                        return "primary";
+                    })
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
                 //
