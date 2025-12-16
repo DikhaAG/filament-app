2c1a9ad4b473e7075ecb2873e5b3ea0fc42fbd01
DikhaAG <dikhaagsfrdy@gmail.com>
2025-12-16 22:41:19 +0700

    filament>resource: membuat resource baru

---
+++ app/Filament/Resources/Tims/Schemas/TimForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Tims\Schemas;
+
+use Filament\Schemas\Schema;
+
+class TimForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
